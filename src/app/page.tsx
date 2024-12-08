70242c9be461afb714b9a9f2dd901676cf5d0542
Ilyacnt <ilyagt2012@gmail.com>
2024-12-08 08:56:28 +0300

    Prisma init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
+import { prisma } from "@/shared/lib/db";
 import { Button } from "@/shared/ui/button";
+import { Card, CardTitle } from "@/shared/ui/card";
 
-export default function Home() {
+export default async function Home() {
+  const games = await prisma.game.findMany();
+
+  console.log(games);
+
   return (
     <div>
+      {games.map((game) => (
+        <Card key={game.id}>
+          <CardTitle>{game.name}</CardTitle>
+        </Card>
+      ))}
       <Button>Click me!</Button>
     </div>
   );
